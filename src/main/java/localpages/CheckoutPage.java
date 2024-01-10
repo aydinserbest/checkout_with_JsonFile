2f86f716c53ba304b06025f72fa35969af167395
aydinSerbest <aydinserbest34@gmail.com>
2024-01-10 15:54:38 +0100

    updated

--- src/main/java/localpages/CheckoutPage.java
+++ src/main/java/localpages/CheckoutPage.java
@@ -1,5 +1,43 @@
 package localpages;
 
-public class CheckoutPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+public class CheckoutPage extends AbstractPage{
+    @FindBy(id = "firstName")
+    private WebElement firstNameInput;
+    @FindBy(id = "lastName")
+    private WebElement lastNameInput;
+    @FindBy(id = "username")
+    private WebElement userNameInput;
+    @FindBy(id = "email")
+    private WebElement emailInput;
+    @FindBy(id = "address")
+    private WebElement address1Input;
+    @FindBy(id = "address2")
+    private WebElement address2Input;
+    @FindBy(id = "country")
+    private WebElement countryInput;
+    @FindBy(id = "state")
+    private WebElement stateInput;
+    @FindBy(id = "zip")
+    private WebElement zipInput;
+
+
+
+    public CheckoutPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @Override
+    public boolean isAt() {
+        return false;
+    }
+    public void selectCountry(String country) {
+        select(countryInput, country);
+    }
+    public void selectState(String state) {
+        select(stateInput, state);
+    }
 }
--- src/test/java/tests/CheckoutTests.java
+++ src/test/java/tests/CheckoutTests.java
@@ -1,4 +1,34 @@
 package tests;
 
-public class CheckoutTests {
+import localpages.CheckoutPage;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import util.Config;
+import util.Constants;
+import util.JsonUtil;
+
+public class CheckoutTests extends AbstractTest {
+    private CheckoutTestData testData;
+
+    @BeforeSuite
+    public void setupConfig() {
+        Config.initialize();
+    }
+
+    @BeforeTest
+    @Parameters("testDataPath")
+    public void setParameters(String testDataPath) {
+        log.info("Test data path received: " + testDataPath);
+        this.testData = JsonUtil.getTestData(testDataPath, CheckoutTestData.class);
+    }
+
+    @Test
+    public void userRegistration() {
+        CheckoutPage checkoutPage = new CheckoutPage(driver);
+        checkoutPage.visit(Config.get(Constants.URL));
+        //other methods
+
+    }
 }
